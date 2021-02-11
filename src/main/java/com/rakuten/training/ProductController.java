21ac072ed1451eb13a6f155540d513d64381325e
akanshat <akanshat1999@gmail.com>
2021-02-11 11:24:13 +0530

    add cors

--- src/main/java/com/rakuten/training/ProductController.java
+++ src/main/java/com/rakuten/training/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,12 +19,12 @@ import com.rakuten.training.domain.Product;
 import com.rakuten.training.service.ProductService;
 import com.rakuten.training.service.ReviewService;
 
+@CrossOrigin
 @RestController
 public class ProductController {
 
 	@Autowired
-	public
-	ProductService service;
+	public ProductService service;
 
 	@Autowired
 	ReviewService reviewSvc;
